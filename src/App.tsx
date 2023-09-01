bf264aa214cae7633d3757a10369c7a8080b1080
SnyDrive25 <sunilgouvi@gmail.com>
2023-09-01 22:37:07 +0200

    Initialization

---
+++ src/App.tsx
@@ -0,0 +1,286 @@
+import { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/modal";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { BrowserRouter, Link } from "react-router-dom";
+import Router from "./Router";
+import RPC from "./web3RPC"; // for using web3.js
+//import RPC from "./ethersRPC"; // for using ethers.js
+
+// Plugins
+import { TorusWalletConnectorPlugin } from "@web3auth/torus-wallet-connector-plugin";
+
+// Adapters
+
+// import { WalletConnectV1Adapter } from "@web3auth/wallet-connect-v1-adapter";
+import {
+  WalletConnectV2Adapter,
+  getWalletConnectV2Settings,
+} from "@web3auth/wallet-connect-v2-adapter";
+import { MetamaskAdapter } from "@web3auth/metamask-adapter";
+import { TorusWalletAdapter } from "@web3auth/torus-evm-adapter";
+
+const clientId = process.env.REACT_APP_CLIENT_ID || "";
+
+console.log("clientId: ", clientId);
+
+function App() {
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [torusPlugin, setTorusPlugin] =
+    useState<TorusWalletConnectorPlugin | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+    null
+  );
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0xaef3",
+            rpcTarget: "https://alfajores-forno.celo-testnet.org",
+          },
+          uiConfig: {
+            appName: "W3A",
+            appLogo: "https://web3auth.io/images/w3a-L-Favicon-1.svg", // Your App Logo Here
+            theme: "light",
+            loginMethodsOrder: ["apple", "google", "twitter"],
+            defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+            loginGridCol: 3,
+            primaryButton: "externalLogin", // "externalLogin" | "socialLogin" | "emailLogin"
+          },
+          web3AuthNetwork: "cyan",
+        });
+
+        const openloginAdapter = new OpenloginAdapter({
+          loginSettings: {
+            mfaLevel: "optional",
+          },
+          adapterSettings: {
+            uxMode: "redirect", // "redirect" | "popup"
+            whiteLabel: {
+              name: "Your app Name",
+              logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+              logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+              defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+              dark: false, // whether to enable dark mode. defaultValue: false
+            },
+            mfaSettings: {
+              deviceShareFactor: {
+                enable: true,
+                priority: 1,
+                mandatory: true,
+              },
+              backUpShareFactor: {
+                enable: true,
+                priority: 2,
+                mandatory: false,
+              },
+              socialBackupFactor: {
+                enable: true,
+                priority: 3,
+                mandatory: false,
+              },
+              passwordFactor: {
+                enable: true,
+                priority: 4,
+                mandatory: false,
+              },
+            },
+          },
+        });
+        web3auth.configureAdapter(openloginAdapter);
+
+        // plugins and adapters are optional and can be added as per your requirement
+        // read more about plugins here: https://web3auth.io/docs/sdk/web/plugins/
+
+        // adding torus wallet connector plugin
+
+        const torusPlugin = new TorusWalletConnectorPlugin({
+          torusWalletOpts: {},
+          walletInitOptions: {
+            whiteLabel: {
+              theme: { isDark: true, colors: { primary: "#00a8ff" } },
+              logoDark: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+              logoLight: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+            },
+            useWalletConnect: true,
+            enableLogging: true,
+          },
+        });
+        setTorusPlugin(torusPlugin);
+        await web3auth.addPlugin(torusPlugin);
+
+        // read more about adapters here: https://web3auth.io/docs/sdk/web/adapters/
+
+        // adding wallet connect v1 adapter
+        // const walletConnectV1Adapter = new WalletConnectV1Adapter({
+        //   adapterSettings: {
+        //     bridge: "https://bridge.walletconnect.org",
+        //   },
+        //   clientId,
+        // });
+
+        // web3auth.configureAdapter(walletConnectV1Adapter);
+
+        // adding wallet connect v2 adapter
+        const defaultWcSettings = await getWalletConnectV2Settings(
+          "eip155",
+          [1, 137, 5],
+          "04309ed1007e77d1f119b85205bb779d"
+        );
+        const walletConnectV2Adapter = new WalletConnectV2Adapter({
+          adapterSettings: { ...defaultWcSettings.adapterSettings },
+          loginSettings: { ...defaultWcSettings.loginSettings },
+        });
+
+        web3auth.configureAdapter(walletConnectV2Adapter);
+
+        // adding metamask adapter
+        const metamaskAdapter = new MetamaskAdapter({
+          clientId,
+          sessionTime: 3600, // 1 hour in seconds
+          web3AuthNetwork: "cyan",
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0xaef3",
+            rpcTarget: "https://alfajores-forno.celo-testnet.org",
+          },
+        });
+        // we can change the above settings using this function
+        metamaskAdapter.setAdapterSettings({
+          sessionTime: 86400, // 1 day in seconds
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0xaef3",
+            rpcTarget: "https://alfajores-forno.celo-testnet.org",
+          },
+          web3AuthNetwork: "cyan",
+        });
+
+        // it will add/update  the metamask adapter in to web3auth class
+        web3auth.configureAdapter(metamaskAdapter);
+
+        const torusWalletAdapter = new TorusWalletAdapter({
+          clientId,
+        });
+
+        // it will add/update  the torus-evm adapter in to web3auth class
+        web3auth.configureAdapter(torusWalletAdapter);
+
+        setWeb3auth(web3auth);
+
+        await web3auth.initModal();
+
+        // await web3auth.initModal({
+        //   modalConfig: {
+        //     [WALLET_ADAPTERS.OPENLOGIN]: {
+        //       label: "openlogin",
+        //       loginMethods: {
+        //         // Disable facebook and reddit
+        //         facebook: {
+        //           name: "facebook",
+        //           showOnModal: false
+        //         },
+        //         reddit: {
+        //           name: "reddit",
+        //           showOnModal: false
+        //         },
+        //         // Disable email_passwordless and sms_passwordless
+        //         email_passwordless: {
+        //           name: "email_passwordless",
+        //           showOnModal: false
+        //         },
+        //         sms_passwordless: {
+        //           name: "sms_passwordless",
+        //           showOnModal: false
+        //         }
+        //       }
+        //     }
+        //   }
+        // });
+        setProvider(web3auth.provider);
+
+        if (web3auth.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      return("web3auth not initialized yet");
+    }
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+    setLoggedIn(true);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      return("web3auth not initialized yet");
+    }
+    await web3auth.logout();
+    setProvider(null);
+    setLoggedIn(false);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      return("web3auth not initialized yet");
+    }
+    const user = await web3auth.getUserInfo();
+    return(user);
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      return("provider not initialized yet");
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    return(address);
+  };
+
+  interface RouterProps {
+    logout: () => void;
+    login: () => void;
+  }
+
+  const Navbar = ({ logout, login }: RouterProps) => {
+    return (
+      <nav>
+          <ul>
+              <li>
+                  <Link to="/">Home</Link>
+                  {
+                      loggedIn ? (
+                          <button onClick={logout}>Logout</button>
+                      ) : (
+                          <button onClick={login}>Login</button>
+                      )
+                  }
+              </li>
+          </ul>
+      </nav>
+    );
+  }
+
+  return (
+    <BrowserRouter>
+      <Navbar logout={logout} login={login} />
+      <Router />
+    </BrowserRouter>
+  );
+}
+
+export default App;
