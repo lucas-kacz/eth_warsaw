1c05e9d04cf04d7e9eb3be7373c188e789cfb7f5
Lucas Kaczmarski <lucaskacz1@gmail.com>
2023-09-02 00:35:33 +0200

    Retrieve user private key

--- src/pages/Request.tsx
+++ src/pages/Request.tsx
@@ -1,37 +1,61 @@
-const { RequestNetwork } = require("@requestnetwork/request-client.js")
-
-const {
-    EthereumPrivateKeySignatureProvider,
-  } = require("@requestnetwork/epk-signature");
-  const { Types } = require("@requestnetwork/request-client.js");
-  
-  const epkSignatureProvider = new EthereumPrivateKeySignatureProvider({
-    method: Types.Signature.METHOD.ECDSA,
-    privateKey: process.env.PAYEE_PRIVATE_KEY, // Must include 0x prefix
-  });
-
-
-const requestClient = new RequestNetwork({
-  nodeConnectionConfig: { 
-    baseURL: "https://goerli.gateway.request.network/",
-  },
-  signatureProvider: epkSignatureProvider,
-});
-
+import { useEffect, useState } from "react";
+import RPC from "../utils/ethersRPC"
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { Web3Auth } from "@web3auth/modal";
 interface RouterProps {
     web3auth: any;
 }
 
-async function create_request(){
-    
-}
-
 const Request = ({ web3auth }: RouterProps) => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+    // const [privateKey, setPrivateKey] = useState("")
+    const { RequestNetwork } = require("@requestnetwork/request-client.js")
+
+    // useEffect(() => {
+    //     getPrivateKey()
+    // }, [])
+
+    const getPrivateKey = async () => {
+        if (!web3auth.provider) {
+            return;
+        }
+        const rpc = new RPC(web3auth.provider);
+        const privateKey = await rpc.getPrivateKey();
+        
+        console.log(privateKey)
+        // setPrivateKey(privateKey)
+    };    
+
+    // const {
+    //     EthereumPrivateKeySignatureProvider,
+    // } = require("@requestnetwork/epk-signature");
+    // const { Types } = require("@requestnetwork/request-client.js");
+    
+    // const epkSignatureProvider = new EthereumPrivateKeySignatureProvider({
+    //     method: Types.Signature.METHOD.ECDSA,
+    //     privateKey: process.env.PAYEE_PRIVATE_KEY, // Must include 0x prefix
+    // });
+
+
+    // const requestClient = new RequestNetwork({
+    // nodeConnectionConfig: { 
+    //     baseURL: "https://goerli.gateway.request.network/",
+    // },
+    // signatureProvider: epkSignatureProvider,
+    // });
+
+    async function create_request(){
+
+    }
+
     return(
         <div>
-
+            {/* {privateKey} */}
+            <button onClick={getPrivateKey}>Get Private Key</button>
         </div>
     )
+
 }
 
 export default Request;
